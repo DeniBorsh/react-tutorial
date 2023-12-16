12e9e6683544f2f0bb83968b90b06ba26dfc65d4
DeniBorsh <b.deni.goperty@yandex.ru>
2023-12-17 02:18:56 +0600

    time 1:30

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import React, {useState, useRef} from 'react'
-import Counter from './components/counter';
-import ClassCounter from './components/classCounter';
+import React, {useState, useRef, useMemo} from 'react'
 import './styles/App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/modal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'React', body: 'Description'},
-    {id: 3, title: 'React Native', body: 'Description'}
+    {id: 1, title: 'Javascript', body: 'Description 3'},
+    {id: 2, title: 'React', body: 'Description 1'},
+    {id: 3, title: 'React Native', body: 'Description 2'}
   ])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
+
+  const sortedPosts = useMemo(() => {
+    if(filter.sort)
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    return posts
+  }, [posts, filter.sort])
+
+  const sortedAndSearchedPosts = useMemo(
+    () => sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase())), 
+    [filter.query, sortedPosts]
+  )
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts([...posts].filter(p => p.id !== post.id))
   }
 
-
   return (
     <div className="App">
-      <PostForm create={createPost}></PostForm>
-      {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title={'Список постов'}/>
-        : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-      }
+      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}></PostForm>
+      </MyModal>
+      <hr style={{margin: '15px 0'}}></hr>
+      <PostFilter filter={filter} setFilter={setFilter}/>
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'}/>
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import PostItem from "./PostItem";
 
 const PostList = ({posts, title, remove}) => {
-    return (
-        <div>
-          <h1 style={{textAlign: 'center'}}>{title}</h1>
-          {posts.map((post, i) => <PostItem remove={remove} number={i+1} post={post}/>)}
-        </div>
-    )
+  if(!posts.length) return <h1 style={{textAlign: 'center'}}>Постов не найдено</h1>
+  
+  return (
+      <div>
+        <h1 style={{textAlign: 'center'}}>{title}</h1>
+        {posts.map((post, i) => <PostItem remove={remove} number={i+1} post={post}/>)}
+      </div>
+  )
 }
 
 export default PostList
